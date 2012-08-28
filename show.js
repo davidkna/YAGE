09354ffe16a4bdc6d2bccf259239bcac7672f241
David Knaack <davidkna@me.com>
2012-08-28 17:52:07 +0200

    bugfix & links now clickable in show.html

--- show.js
+++ show.js
@@ -13,11 +13,28 @@ $(function ($) {
 		$('#cnt').prepend('<h1>' + getUrlVar('s') + '</h1>\n');
 		$('#cnt img').each(function () {
 			var $this = $(this);
-			if ($this.attr('src')[0] == '/') $this.attr('src', 'http://www.glitch-strategy.com/' + $this.attr('src'))
+			if ($this.attr('src')[0] == '/') $this.attr('src', 'http://www.glitch-strategy.com' + $this.attr('src'))
 		});
 		$('#cnt a').each(function () {
-			var $this = $(this);
-			if ($this.attr('href')[0] == '/') $this.attr('href', 'http://www.glitch-strategy.com/' + $this.attr('href'))
+			var $this = $(this),
+				oHref = this.href.split('/');
+			if ( oHref[0] == 'chrome-extension:' ) {
+				if ( oHref[3] == 'wiki') this.href = 'show.html?s=' + $(this).html();
+				else {
+					$(this).on("click", function (event) {
+						chrome.tabs.create({
+							url: 'http://www.glitch-strategy.com' + this.pathname
+						});
+					});
+					this.href = 'http://www.glitch-strategy.com' + this.pathname;
+				}
+		} else {
+			$(this).on("click", function (event) {
+				chrome.tabs.create({
+					url: $(this).attr('href')
+				});
+			});
+		}
 		});
 	});
 });

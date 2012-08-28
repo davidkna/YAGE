ca3f020c472fb8f830fb84fcbd9cb7985b7aff9c
David Knaack <davidkna@me.com>
2012-08-28 16:57:22 +0200

    1.2.2
    more btn classes, bootstrap 2.1, fix serious routes

--- routes.js
+++ routes.js
@@ -1,20 +1,25 @@
 $(function ($) {
-	$.get('http://seriousroutes.com/', function (rp) {
-		var div = $('<div></div>');
-		$(div).append('<h2 id=seriousroutes><a href="http://seriousroutes.com/">Serious Routes</a></h2>\n');
-		$(div).append($('<div></div>').html($(b = rp)).find('#routes').html());
-		$('#routes').append(div);
-	});
-	$.get('http://resources.grelca.com/route', function (rp) {
-		$('#routes').append('<h2 id=grelca><a href="http://resources.grelca.com/">Housing Routes &amp; Directory</a></h2>\n' + $('<div></div>').html($(rp)).find("#route-list").html());
-		$('.group').addClass('label label-info');
-	});
 	$.get('http://glitchroutes.com/build/', function (rp) {
 		var $rp = $('<div></div>').html($(rp));
 		$rp.find('form').attr('action', 'http://glitchroutes.com/build/').removeAttr('style').wrap('<div class="form"></div>');
-		$('#routes').append('<h2 id=build><a href="http://glitchroutes.com/build/">Build Route</a></h2>\n' + $rp.find(".form").eq(0).html() + '<hr/>'+ $rp.find(".form").eq(1).html());
+		$('#build').append('<h2><a href="http://glitchroutes.com/build/">Build Route</a></h2>\n' + $rp.find(".form").eq(0).html() + '<hr/>'+ $rp.find(".form").eq(1).html());
+		$('#build input[type="submit"]').addClass('btn');
 	});
-	$('.star').remove()
+	$.get('http://seriousroutes.com/', function (rp) {
+		var div = $('<div></div>'),
+			$rp = $(rp);
+		$rp.find('button').addClass('btn');
+		$(div).append('<h2 id=seriousroutes><a href="http://seriousroutes.com/">Serious Routes</a></h2>\n' + $('<div></div>').html($rp).find('#routes').html());
+		$('#serious').append(div);
+		$('#serious button').addClass('btn');
+	});
+	$.get('http://resources.grelca.com/route', function (rp) {
+		$('#grelca').append('<h2 id=grelca><a href="http://resources.grelca.com/">Housing Routes &amp; Directory</a></h2>\n' + $('<div></div>').html($(rp)).find("#route-list").html());
+		$('.group').addClass('label label-info');
+		$('#grelca input[type="submit"]').addClass('btn');
+	});
+	$('.star').remove();
+	$('button, input[type="submit"]').addClass('btn');
 	$('#routes').on("click", "a:not(.btn)", function (event) {
 		chrome.tabs.create({
 			url: $(this).attr('href')

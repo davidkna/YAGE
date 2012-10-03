e4e2fef4923b0ef4c1222d7268bdf7bade957999
David Knaack <davidkna@me.com>
2012-10-03 16:03:17 +0200

    improve table display & other stuff
    Other Stuff:
    -better human readable readme.md url
    -remove stars (in routes)
    -better quotes
    -cleaner code

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ http://github.com/davidkna/YAGE/
 
 License: CC BY-SA Germany 3.0 http://creativecommons.org/licenses/by-sa/3.0/de/
 
-Download: https://chrome.google.com/webstore/detail/mfcipeeciejebgakmlmbbpmgokdeekig/
+Download: https://chrome.google.com/webstore/detail/yage-yet-another-glitch-e/mfcipeeciejebgakmlmbbpmgokdeekig
--- popup.js
+++ popup.js
@@ -4,48 +4,89 @@ $(function ($) {
 		var result = '';
 		for (var i = 0; i < response.count; i++) {
 			var item = response.value.items[i];
-			result += '<div class="item">\n<h3><a href="' + item['feedburner:origLink'] + '">' + item.title + '</h2>\nPosted <time>' + item.pubDate.replace(' +0000', '') + '</time><p>\n' + '<img src="' + $(item['content:encoded']).find('img').attr('src') + '">' + item.description + '</p>\n</a></div>\n';
+			result += '<div class="item">\n<h3><a href="' + item['feedburner:origLink'] + '">' + item.title + '</h2>\nPosted <time>' + item.pubDate.replace(' +0000', '') + '</time><p>\n' + '<img src="' + $(item['content:encoded'])
+				.find('img')
+				.attr('src') + '">' + item.description + '</p>\n</a></div>\n';
 		};
-		$('.feed').append(result);
+		$('.feed')
+			.append(result);
 	});
-	$('.feed').on("click", "a", function (event) {
+	$('.feed')
+		.on("click", "a", function (event) {
 		chrome.tabs.create({
-			url: $(this).attr('href')
+			url: $(this)
+				.attr('href')
 		});
 	});
 
 	// Routes
-	$('a[data-toggle="tab"]').eq(2).on('shown', function (e) {
+	$('a[data-toggle="tab"]')
+		.eq(2)
+		.on('shown', function (e) {
 		$.get('http://glitchroutes.com/build/', function (rp) {
-			var $rp = $('<div></div>').html($(rp.replace(/<script[ >][\s\S]*?<\/script>/g, '')));
-			$rp.find('form').attr('action', 'http://glitchroutes.com/build/').removeAttr('style').wrap('<div class="form"></div>');
-			$('#build').append($rp.find(".form").eq(0).html() + '<hr/>' + $rp.find(".form").eq(1).html());
-			$('#build input[type="submit"]').addClass('btn');
-			$('#build').find('.loading').remove();
+			var $rp = $('<div></div>')
+				.html($(rp.replace(/<script[ >][\s\S]*?<\/script>/g, '')));
+			$rp.find('form')
+				.attr('action', 'http://glitchroutes.com/build/')
+				.removeAttr('style')
+				.wrap('<div class="form"></div>');
+			$('#build')
+				.append($rp.find(".form")
+				.eq(0)
+				.html() + '<hr/>' + $rp.find(".form")
+				.eq(1)
+				.html());
+			$('#build input[type="submit"]')
+				.addClass('btn');
+			$('#build')
+				.find('.loading')
+				.remove();
 		});
 		$.get('http://seriousroutes.com/', function (rp) {
 			var div = $('<div></div>'),
 				$rp = $(rp.replace(/<script[ >][\s\S]*?<\/script>/g, '')),
 				$srs = $('#serious');
-			$(div).append($('<div></div>').html($rp).find('#routes').html());
+			$(div)
+				.append($('<div></div>')
+				.html($rp)
+				.find('#routes')
+				.html());
 			$srs.append(div);
-			$srs.find('form').each(function () {
-				$(this).attr('action', 'http://www.glitch.com/locations/' + $(this).attr('action'));
-				$(this).find('button').addClass('btn');
+			$srs.find('form')
+				.each(function () {
+				$(this)
+					.attr('action', 'http://www.glitch.com/locations/' + $(this)
+					.attr('action'));
+				$(this)
+					.find('button')
+					.addClass('btn');
 			});
-			$srs.find('.loading').remove();
+			$srs.find('.loading')
+				.remove();
 		});
 		$.get('http://resources.grelca.com/route', function (rp) {
-			$('#grelca').append($('<div></div>').html($(rp.replace(/<script[ >][\s\S]*?<\/script>/g, ''))).find("#route-list").html());
-			$('.group').addClass('label label-info');
-			$('#grelca input[type="submit"]').addClass('btn');
-			$('#grelca').find('.loading').remove();
+			$('#grelca')
+				.append($('<div></div>')
+				.html($(rp.replace(/<script[ >][\s\S]*?<\/script>/g, '')))
+				.find("#route-list")
+				.html());
+			$('.group')
+				.addClass('label label-info');
+			$('#grelca input[type="submit"]')
+				.addClass('btn');
+			$('#grelca')
+				.find('.loading')
+				.remove();
 		});
-		$('.star').remove();
-		$('button, input[type="submit"]').addClass('btn');
-		$('.tab-content').on('a', 'click', function (event) {
+		$('.star')
+			.remove();
+		$('button, input[type="submit"]')
+			.addClass('btn');
+		$('.tab-content')
+			.on('a', 'click', function (event) {
 			chrome.tabs.create({
-				url: $(this).attr('href')
+				url: $(this)
+					.attr('href')
 			});
 		});
 	});
@@ -53,7 +94,8 @@ $(function ($) {
 	// Search Glitch Strategy
 
 	function getUrlVar(key) {
-		var result = new RegExp(key + "=([^&]*)", "i").exec(window.location.search);
+		var result = new RegExp(key + "=([^&]*)", "i")
+			.exec(window.location.search);
 		return decodeURIComponent(result && result[1] || "");
 	}
 
@@ -69,47 +111,77 @@ $(function ($) {
 	}
 
 	function openWiki(page) {
-		$('#cnt').html('<img src="images/ajax-loader.gif" alt="loading..." class="loading">');
+		$('#cnt')
+			.html('<img src="images/ajax-loader.gif" alt="loading..." class="loading">');
 		$.getJSON('http://www.glitch-strategy.com/w/api.php', {
 			'action': 'parse',
 			'format': 'json',
 			'page': page,
 			'prop': 'text'
 		}, function (response) {
-			$('base').attr('href', 'http://www.glitch-strategy.com');
-			$('#cnt').empty().append('<hr><h1>' + page.replace(new RegExp('_', 'g'), ' ') + '</h1>\n' + response.parse.text['*'].replace(new RegExp('href="/wiki/', 'g'), 'data-wiki="1" href="http://glitch-strategy.com/wiki/'));
+			$('base')
+				.attr('href', 'http://www.glitch-strategy.com');
+			$('#cnt')
+				.empty()
+				.append('<hr><h1>' + page.replace(new RegExp('_', 'g'), ' ') + '</h1>\n' + response.parse.text['*'].replace(new RegExp('href="/wiki/', 'g'), 'data-wiki="1" href="http://glitch-strategy.com/wiki/'));
+			$('#cnt *')
+				.removeAttr('style');
+			$('table')
+				.addClass('table table-bordered');
 			localStorage.clear();
-			$('#cnt a').not('a[data-wiki="1"]').each(function () {
+			$('#cnt a')
+				.not('a[data-wiki="1"]')
+				.each(function () {
 				var $this = $(this),
 					href = this.href.split('/');
 				if (href[0] != 'chrome-extension:') {
-					$(this).on("click", function (event) {
+					$(this)
+						.on("click", function (event) {
 						chrome.tabs.create({
-							url: $(this).attr('href')
+							url: $(this)
+								.attr('href')
 						});
-					});
-				}
-			});
-			$('base').attr('href', '');
-		});
-	}
-	if (getUrlVar('s')) search(getUrlVar('s'));
-	$('#s').keypress(function () {
-		$('#s').typeahead({
-					'source': search
-		});
-	});
-	$('#cnt').on('click', 'a[data-wiki="1"]', function () {
-		var page = this.href.split('/')[4];
-		$('#s').val(page.replace(new RegExp('_', 'g'), ' '));
-		openWiki(page);
-	})
-	$('form').eq(0).submit(function (e) {
-		openWiki($('#s').val());
-		return false;
-	});
 
-});
+					}
+					});
+				$('.cquote')
+					.each(function () {
+					$(this)
+						.after('<blockquote>' + $(this)
+						.find('td')
+						.eq(1)
+						.html() + '</blockquote>');
+					$(this)
+						.remove()
+				})
+				$('base')
+					.attr('href', '');
+			});
+		}
+		if (getUrlVar('s')) search(getUrlVar('s'));
+		$('#s')
+			.keypress(function () {
+			$('#s')
+				.typeahead({
+				'source': search
+			});
+		});
+		$('#cnt')
+			.on('click', 'a[data-wiki="1"]', function () {
+			var page = this.href.split('/')[4];
+			$('#s')
+				.val(page.replace(new RegExp('_', 'g'), ' '));
+			openWiki(page);
+		})
+		$('form')
+			.eq(0)
+			.submit(function (e) {
+			openWiki($('#s')
+				.val());
+			return false;
+		});
+
+		});
 /**
 * Bootstrap.js by @fat & @mdo
 * plugins: bootstrap-tab.js, bootstrap-typeahead.js
